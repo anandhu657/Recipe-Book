bb93e72a3ee5b94e7382987698458e1a961d9c5c
Anandhu M S <anandhumeleveettil@gmail.com>
2023-06-13 20:28:27 +0530

    model moved to shared folder

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Ingredient } from 'src/app/models/ingredient.model';
+import { Ingredient } from 'src/app/shared/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
